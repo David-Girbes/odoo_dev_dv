429ddb85684cc10040a069b77caf5749a75cabb6
David <davgirher@alu.edu.gva.es>
2025-12-19 13:05:28 +0100

    hola

--- data/addons/gestion_restaurante_david/models/models.py
+++ data/addons/gestion_restaurante_david/models/models.py
@@ -2,16 +2,55 @@ from odoo import models, fields, api
 
 
 class plato_david(models.Model):
-    _name = 'gestion_restaurante_david.gestion_restaurante_david'
+    _name = 'gestion_restaurante_david.plato_david'
+    _description = 'suwiii'
+
+    name = fields.Char(
+        string="Nombre",
+        help="nombre del plato",
+        required=True
+
+    )
+    descripcion = fields.Text(
+        string="Descripcion",
+        help="desc del plato",
+    )
+
+    precio = fields.Float(
+        string="Precio",
+        help="precio del plato",
+        required=True
+    )
+    tiempo_preparacion = fields.Integer(
+        string="Tiempo",
+        help="tiempo del plato",
+        required=True
+    )
+    disponible = fields.Boolean(
+        string="Disponible",
+        help="disponibilidad del plato",
+        default=True)
+    
+    categoria = fields.Selection(
+        [
+            ('entrante','Entrante'),
+            ('principal','Principal'),
+            ('postre','Postre'),
+            ('bebida','Bebida')
+        ],
+        string="Selección",
+        help="nombre del plato",
+        required=True
+    )
+
+
+class menu_david(models.Model):
+    _name = 'gestion_restaurante_david.menu_david'
     _description = 'gestion_restaurante_david.gestion_restaurante_david'
 
-    nombre = fields.Char()
-    descripcion = fields.Text()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+
+
+class ingredientes_david(models.Model):
+    _name = 'gestion_restaurante_david.ingredientes_david'
+    _description = 'gestion_restaurante_david.gestion_restaurante_david'
 
--- data/addons/gestion_tareas_david/models/models.py
+++ data/addons/gestion_tareas_david/models/models.py
@@ -1,18 +1,122 @@
+from datetime import timedelta
 from odoo import models, fields, api
 
 
-class gestion_tareas_david(models.Model):
-    _name = 'gestion_tareas_david.gestion_tareas_david'
-    _description = 'gestion_tareas_david.gestion_tareas_david'
+class tareas_david(models.Model):
+    _name = 'gestion_tareas_david.tareas_david'
+    _description = 'gestion_tareas_david.tareas_david'
+
+    # En el modelo tareas_sergio
+    codigo = fields.Char(compute="_get_codigo") 
 
     nombre = fields.Char()
+    
     descripcion = fields.Text()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
 
+    fecha_ini = fields.Datetime(
+        string="Fecha Inicio", 
+        required=True, 
+        help="Fecha y hora de inicio del sprint")
+    
+    fecha_creacion = fields.Date(
+        string="Fecha Creación", 
+        required=True, 
+        help="Fecha en la que se dio de alta la tarea")
+
+    fecha_fin = fields.Datetime(
+        string="Fecha Final", 
+        store=True,
+        help="Fecha y hora de finalización del sprint")
+    
+    finalizado = fields.Boolean(
+        string="Finalizado", 
+        help="Indica si la tarea ha sido finalizada o no")
+    
+    sprint = fields.Many2one(
+        'gestion_tareas_david.sprints_david', 
+        string='Sprint relacionado', 
+        ondelete='set null', 
+        help='Sprint al que pertenece esta tarea')
+    
+    rel_tecnologias = fields.Many2many(
+        comodel_name='gestion_tareas_david.tecnologias_david',
+        relation='relacion_tareas_tecnologias',
+        column1='rel_tareas',
+        column2='rel_tecnologias',
+        string='Tecnologías')
+    
+    def _get_codigo(self):
+        for tarea in self:
+            # Si la tarea no tiene un sprint asignado
+            if not tarea.sprint:
+                tarea.codigo = "TSK_" + str(tarea.id)
+            else:
+                # Si tiene sprint, usamos su nombre
+                tarea.codigo = str(tarea.sprint.name).upper() + "_" + str(tarea.id)
+    
+    
+class sprints_david(models.Model):
+    _name = 'gestion_tareas_david.sprints_david'
+    _description = 'Modelo de Sprints para Gestión de Proyectos'
+
+    name = fields.Char(
+        string="Nombre", 
+        required=True, 
+        help="Introduzca el nombre del sprint")
+
+    descripcion = fields.Text(
+        string="Descripción", 
+        help="Breve descripción del sprint")
+
+    fecha_ini = fields.Datetime(
+        string="Fecha Inicio", 
+        required=True, 
+        help="Fecha y hora de inicio del sprint")
+    
+    duracion = fields.Integer(
+        string="Duración", 
+        help="Cantidad de días que tiene asignado el sprint")
+
+    fecha_fin = fields.Datetime(
+        compute='_compute_fecha_fin', 
+        store=True,
+        string="Fecha Fin")
+
+    @api.depends('fecha_ini', 'duracion')
+    def _compute_fecha_fin(self):
+        for sprint in self:
+            if sprint.fecha_ini and sprint.duracion and sprint.duracion > 0:
+                sprint.fecha_fin = sprint.fecha_ini + timedelta(days=sprint.duracion)
+            else:
+                sprint.fecha_fin = sprint.fecha_ini
+
+    tareas = fields.One2many(
+        'gestion_tareas_david.tareas_david', 
+        'sprint', 
+        string='Tareas del Sprint')
+    
+    class tecnologias_david(models.Model):
+        _name = 'gestion_tareas_david.tecnologias_david'
+        _description = 'Modelo de Tecnologías'
+
+        name = fields.Char(
+            string="Nombre", 
+            required=True, 
+            help="Nombre de la tecnología")
+
+        descripcion = fields.Text(
+            string="Descripción", 
+            help="Breve descripción de la tecnología")
+
+        logo = fields.Image(
+            string="Logo", 
+            max_width=256, 
+            max_height=256,
+            help="Logo de la tecnología")
+        
+        rel_tareas = fields.Many2many(
+            comodel_name='gestion_tareas_david.tareas_david',
+            relation='relacion_tareas_tecnologias',
+            column1='rel_tecnologias',
+            column2='rel_tareas',
+            string='Tareas')

839911d21afbb158093946d2a08dae7f15b446aa
David <davgirher@alu.edu.gva.es>
2026-02-13 13:44:54 +0100

    Dia 4

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ He hecho los modelos de dronify, contacto y un poco las vistas de estas
 ## Dia 2
 
 Los modelos están acabados con las relaciones, solo faltan algunos campos computados y he mejorado un poco las vistas 
+
+## Dia 3
+
+Avanzar las vistas de piloto
+
+## Dia 4
+
+He hecho toda la lógica de todos los campos, y acabado del todo las vistas de res.partner, vista de vuelo
--- data/addons/dronify/models/models.py
+++ data/addons/dronify/models/models.py
@@ -1,6 +1,7 @@
 import datetime
 from odoo import models,fields,api
-# from logica_dronify import calcular_consumo_vuelo,validar_estado_bateria
+from .logica_dronify import calcular_consumo_vuelo,validar_estado_bateria
+from odoo.exceptions import ValidationError
 
 
 #DRON---------------------------------------------------------
@@ -38,7 +39,8 @@ class dron(models.Model):
         'pilotos_drones_rel',
         column1="dron_id",
         column2="piloto_id",
-        string="Pilotos"
+        string="Pilotos",
+        domain=[('es_piloto','=',True)]
     )
 
 
@@ -57,9 +59,7 @@ class contacto(models.Model):
     )
 
     es_piloto = fields.Boolean(
-        string="Es Piloto",
-        defualt=True,
-        readonly=True
+        string="Es Piloto"
     )
 
     licencia = fields.Char(
@@ -114,11 +114,12 @@ class paquete(models.Model):
         related="vuelo_id.dron_id.name"
     )
 
+    @api.depends('create_date')
     def _compute_codigo(self):
         for paquete in self:
-            ahora = fields.Datetime.now() 
-            fecha_str = ahora.strftime("%Y%m%d%H%M%S")
-            paquete.codigo = f"{fecha_str}"
+            fecha_str = paquete.create_date or fields.Datetime.now()
+            paquete.codigo = fecha_str.strftime("%Y%m%d%H%M%S")
+            
 
 #VUELO---------------------------------------------------------
 class vuelo(models.Model):
@@ -128,7 +129,7 @@ class vuelo(models.Model):
     codigo = fields.Char(
         compute="_compute_codigo",
         string="Código",
-        readonly=True
+        readonly=True,
     )
 
     def _compute_name_default(self):
@@ -167,7 +168,8 @@ class vuelo(models.Model):
     piloto_id = fields.Many2one(
         'res.partner',
         string="Piloto",
-        ondelete="set null"
+        ondelete="set null",
+        domain=[('es_piloto','=',True)]
     )
 
     peso_total = fields.Float(
@@ -176,17 +178,62 @@ class vuelo(models.Model):
         store=True
     )
 
-    # consumo_estimado = fields.Float(
-    #     string="Consumo",
-    #     compute="calcular_consumo_vuelo",
-    #     store=True
-    # )
+    consumo_estimado = fields.Float(
+        string="Consumo",
+        compute="_compute_consumo",
+        store=True
+    )
 
+    @api.depends('paquetes_ids.peso')
+    def _compute_consumo(self):
+        for vuelo in self:
+            vuelo.consumo_estimado = calcular_consumo_vuelo(vuelo.peso_total)
+        
+    @api.depends('create_date')
     def _compute_codigo(self):
         for vuelo in self:
-            ahora = fields.Datetime.now() 
-            fecha_str = ahora.strftime("%Y%m%d%H%M%S")
-            vuelo.codigo = f"{fecha_str}"
+            if vuelo.create_date:
+                fecha_str = vuelo.create_date.strftime('%Y%m%d%H%M%S')
+                vuelo.codigo = f"{fecha_str}"
+            
+
+    def preparar_vuelo(self):
+        for vuelo in self:
+                
+            if not vuelo.piloto_id and not vuelo.dron_id:
+                raise ValidationError("Tienes que poner dron y piloto")
+            
+            if not vuelo.paquetes_ids:
+                raise ValidationError("Tiene que tener algún paquete")
+            
+            if vuelo.peso_total > vuelo.dron_id.capacidad_max:
+                raise ValidationError("El peso es superior a la capacidad del dron")
+            
+            if vuelo.dron_id.estado != 'disponible':
+                raise ValidationError("El dron debe estar disponible")
+
+            if not validar_estado_bateria(vuelo.dron_id.bateria, vuelo.consumo_estimado):
+                raise ValidationError("La batería debe ser mayor al consumo")
+            
+            if vuelo.dron_id not in vuelo.piloto_id.dron_autorizado_ids:
+                raise ValidationError("Ese piloto no tiene ese dron asignado")
+            
+            vuelo.preparado = True
+
+            vuelo.dron_id.estado = 'vuelo'
+
+    def desbloquear(self):
+        for vuelo in self:
+            vuelo.preparado = False
+            vuelo.dron_id.estado = 'disponible'
+
+    def realizar_vuelo(self):
+        for vuelo in self:
+            vuelo.dron_id.bateria = vuelo.dron_id.bateria - vuelo.consumo_estimado
+            vuelo.dron_id.estado = 'disponible'
+
+            vuelo.realizado = True
+
 
     @api.depends('paquetes_ids.peso')
     def _compute_peso_total(self):
